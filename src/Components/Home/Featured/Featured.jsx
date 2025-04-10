83c09fee4d517379cb720728f0590b6e226c4137
Chowon-Hasan <chowonhasan7@gmail.com>
2025-04-10 23:34:19 +0600

    dfdfsdfs

--- src/Components/Home/Featured/Featured.jsx
+++ src/Components/Home/Featured/Featured.jsx
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from "react";
 import Image from "next/image";
 import VerticalSlider from "@/Components/VerticalSlider";
 import Link from "next/link";
+import { FaFilter } from "react-icons/fa";
 import ProductCardSkeleton from "./CardSkeleton";
 
 const Featured = () => {
@@ -115,100 +116,200 @@ const Featured = () => {
             className="border border-red-500 text-sm rounded-lg p-2 w-full"
           />
         </div>
-        <div className="md:w-3/5 md:mx-auto flex md:justify-between justify-center flex-wrap mt-4">
-          <select
-            name="brand"
-            onChange={handleFilterChange}
-            value={filters.brand}
-            className="border border-red-500 text-sm rounded-lg px-2 mr-2 mb-2 md:mb-0"
-          >
-            <option value="">Marque</option>
-            {brands.map((brand, index) => (
-              <option key={index} value={brand}>
-                {brand}
-              </option>
-            ))}
-          </select>
+        <div className="hidden md:block">
+          <div className="md:w-3/5 md:mx-auto grid grid-cols-6 gap-2 md:justify-between justify-between flex-wrap mt-4 ">
+            <select
+              name="brand"
+              onChange={handleFilterChange}
+              value={filters.brand}
+              className="border border-red-500 text-sm rounded-lg px-2 mr-2 mb-2 md:mb-0 w-full"
+            >
+              <option value="">Marque</option>
+              {brands.map((brand, index) => (
+                <option key={index} value={brand}>
+                  {brand}
+                </option>
+              ))}
+            </select>
 
-          <select
-            name="model"
-            onChange={handleFilterChange}
-            value={filters.model}
-            className="border border-red-500 text-sm rounded-lg px-2  mr-2 mb-2 md:mb-0"
-          >
-            <option value="">Modèle</option>
-            {models.map((model, index) => (
-              <option key={index} value={model}>
-                {model}
-              </option>
-            ))}
-          </select>
+            <select
+              name="model"
+              onChange={handleFilterChange}
+              value={filters.model}
+              className="border border-red-500 text-sm rounded-lg px-2  mr-2 mb-2 md:mb-0 w-full"
+            >
+              <option value="">Modèle</option>
+              {models.map((model, index) => (
+                <option key={index} value={model}>
+                  {model}
+                </option>
+              ))}
+            </select>
 
-          <select
-            name="fuel"
-            onChange={handleFilterChange}
-            value={filters.fuel}
-            className="border border-red-500 text-sm rounded-lg px-2  mr-2 mb-2 md:mb-0"
-          >
-            <option value="">Energie</option>
-            {fuels.map((fuel, index) => (
-              <option key={index} value={fuel}>
-                {fuel}
-              </option>
-            ))}
-          </select>
+            <select
+              name="fuel"
+              onChange={handleFilterChange}
+              value={filters.fuel}
+              className="border border-red-500 text-sm rounded-lg px-2  mr-2 mb-2 md:mb-0 w-full"
+            >
+              <option value="">Energie</option>
+              {fuels.map((fuel, index) => (
+                <option key={index} value={fuel}>
+                  {fuel}
+                </option>
+              ))}
+            </select>
 
-          <select
-            name="gearbox"
-            onChange={handleFilterChange}
-            value={filters.gearbox}
-            className="border border-red-500 text-sm rounded-lg px-2  mr-2 mb-2 md:mb-0"
-          >
-            <option value="">Transmission</option>
-            {gearboxes.map((gearbox, index) => (
-              <option key={index} value={gearbox}>
-                {gearbox}
-              </option>
-            ))}
-          </select>
+            <select
+              name="gearbox"
+              onChange={handleFilterChange}
+              value={filters.gearbox}
+              className="border border-red-500 text-sm rounded-lg px-2  mr-2 mb-2 md:mb-0 w-full"
+            >
+              <option value="">Transmission</option>
+              {gearboxes.map((gearbox, index) => (
+                <option key={index} value={gearbox}>
+                  {gearbox}
+                </option>
+              ))}
+            </select>
 
-          <select
-            name="location"
-            onChange={handleFilterChange}
-            value={filters.location}
-            className="border border-red-500 text-sm rounded-lg px-2  mr-2 mb-2 md:mb-0"
-          >
-            <option value="">Localisation</option>
-            {locations.map((location, index) => (
-              <option key={index} value={location}>
-                {location}
-              </option>
-            ))}
-          </select>
+            <select
+              name="location"
+              onChange={handleFilterChange}
+              value={filters.location}
+              className="border border-red-500 text-sm rounded-lg px-2  mr-2 mb-2 md:mb-0 w-full"
+            >
+              <option value="">Localisation</option>
+              {locations.map((location, index) => (
+                <option key={index} value={location}>
+                  {location}
+                </option>
+              ))}
+            </select>
 
-          <button
-            onClick={clearFilters}
-            className="border border-red-500 text-sm rounded-lg px-5 bg-red-500 text-white hover:bg-red-600 transition"
-          >
-            Remise à 0
-          </button>
+            <button onClick={clearFilters}>
+              <p className="border border-red-500 text-sm rounded-lg py-[2px] px-5 bg-red-500 text-white hover:bg-red-600 transition w-full">
+                Remise à 0
+              </p>
+            </button>
+          </div>
         </div>
       </div>
 
-      {/* Selected Filters */}
-      <div className="text-xs text-gray-600 mb-4">
-        Filtres séléctionnés:{" "}
-        {Object.entries(filters).map(([key, value]) => {
-          if (value) {
-            return (
-              <span key={key} className="mr-2">
-                {key}: {value}
-              </span>
-            );
-          }
-          return null;
-        })}
+      <div className="flex justify-between items-center">
+        {/* Selected Filters */}
+        <div className="text-xs text-gray-600 mb-4">
+          Filtres séléctionnés:{" "}
+          {Object.entries(filters).map(([key, value]) => {
+            if (value) {
+              return (
+                <span key={key} className="mr-2">
+                  {key}: {value}
+                </span>
+              );
+            }
+            return null;
+          })}
+        </div>
+        {/* Open the modal using document.getElementById('ID').showModal() method */}
+        <button
+          className="text-xs text-gray-600 mb-4 flex items-center gap-2 md:hidden "
+          onClick={() => document.getElementById("my_modal_2").showModal()}
+        >
+          <p>Filtres</p>
+          <FaFilter className="text-red-500" />
+        </button>
       </div>
+
+      {/* Responsive filters */}
+      <div>
+        <dialog id="my_modal_2" className="modal">
+          <div className="modal-box bg-white">
+            <div className="grid grid-cols-2 gap-3">
+              <select
+                name="brand"
+                onChange={handleFilterChange}
+                value={filters.brand}
+                className="border border-red-500 text-sm rounded-lg px-2 mr-2 mb-2 md:mb-0"
+              >
+                <option value="">Marque</option>
+                {brands.map((brand, index) => (
+                  <option key={index} value={brand}>
+                    {brand}
+                  </option>
+                ))}
+              </select>
+
+              <select
+                name="model"
+                onChange={handleFilterChange}
+                value={filters.model}
+                className="border border-red-500 text-sm rounded-lg px-2  mr-2 mb-2 md:mb-0"
+              >
+                <option value="">Modèle</option>
+                {models.map((model, index) => (
+                  <option key={index} value={model}>
+                    {model}
+                  </option>
+                ))}
+              </select>
+
+              <select
+                name="fuel"
+                onChange={handleFilterChange}
+                value={filters.fuel}
+                className="border border-red-500 text-sm rounded-lg px-2  mr-2 mb-2 md:mb-0"
+              >
+                <option value="">Energie</option>
+                {fuels.map((fuel, index) => (
+                  <option key={index} value={fuel}>
+                    {fuel}
+                  </option>
+                ))}
+              </select>
+
+              <select
+                name="gearbox"
+                onChange={handleFilterChange}
+                value={filters.gearbox}
+                className="border border-red-500 text-sm rounded-lg px-2  mr-2 mb-2 md:mb-0"
+              >
+                <option value="">Transmission</option>
+                {gearboxes.map((gearbox, index) => (
+                  <option key={index} value={gearbox}>
+                    {gearbox}
+                  </option>
+                ))}
+              </select>
+
+              <select
+                name="location"
+                onChange={handleFilterChange}
+                value={filters.location}
+                className="border border-red-500 text-sm rounded-lg px-2  mr-2 mb-2 md:mb-0"
+              >
+                <option value="">Localisation</option>
+                {locations.map((location, index) => (
+                  <option key={index} value={location}>
+                    {location}
+                  </option>
+                ))}
+              </select>
+
+              <button onClick={clearFilters}>
+                <p className="text-white bg-red-500 text-sm rounded-lg px-2 py-[2px]  mr-2 mb-2 md:mb-0">
+                  Remise à 0
+                </p>
+              </button>
+            </div>
+          </div>
+          <form method="dialog" className="modal-backdrop">
+            <button>close</button>
+          </form>
+        </dialog>
+      </div>
+      {/* Responisve Filters */}
 
       {/* Main Logic */}
       {cardLoad ? (
